3dbd5abd3c977a82d82b77af4ff58ac386c20bc5
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-02-26 21:27:24 -0800

    Changed comments.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
+/*
+    Danielle Smith
+    script.js -- creates randomly generated string with selection based on user input, pushes it to the "viable_passwords" list in index.html
 
-var password_length = 0, floor = 32, ceiling = 0;
+*/
+
+
+
 
 
 $("#submit").on("click", function (e) {   
@@ -8,14 +14,12 @@ $("#submit").on("click", function (e) {
 
     if($("#length").val() < 8 || password_length > 128){
             alert("Password length invalid! Please input a value between 8 and 128.");
-            // password_length = 0;
     }
     else{
 
 
         //Populate special characters
         if($("#yes_special").prop("checked") == true){
-            console.log("user wants special characters.");
             for(i = " ".charCodeAt(); i <= "/".charCodeAt(); i++){
                 chars.push(String.fromCharCode(i));
             }
@@ -24,9 +28,7 @@ $("#submit").on("click", function (e) {
 
         //Populate numbers
         if($("#yes_numbers").prop("checked") == true){
-         console.log("user wants numeric characters.");
              for(i = "0".charCodeAt(); i <= "9".charCodeAt(); i++){
-                console.log(i);
                 chars.push(String.fromCharCode(i));
             }
         }
@@ -34,9 +36,7 @@ $("#submit").on("click", function (e) {
 
         //Populate uppercase letters
          if($("#yes_upper").prop("checked") == true){
-             console.log("user wants upper case.");
              for(i = "A".charCodeAt(); i <= "Z".charCodeAt(); i++){
-                console.log(i);
                  chars.push(String.fromCharCode(i));
              }
         }
@@ -44,9 +44,7 @@ $("#submit").on("click", function (e) {
 
         //Populate lowercase letters
         if($("#yes_lower").prop("checked") == true){
-            console.log("user wants lower case.");
             for(i = "a".charCodeAt(); i <= "z".charCodeAt(); i++){
-                console.log(i);
                 chars.push(String.fromCharCode(i));
              }
         }
